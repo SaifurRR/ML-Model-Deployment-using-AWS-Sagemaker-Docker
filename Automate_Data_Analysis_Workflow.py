9cec25ecece4b3a873f1137e0056bf3dfbb4722d
SaifurR <saifur.rahman@ubc.ca>
2025-02-18 22:15:39 -0800

    Update Automate_Data_Analysis_Workflow.py

--- Automate_Data_Analysis_Workflow.py
+++ Automate_Data_Analysis_Workflow.py
@@ -3,7 +3,7 @@ import numpy as np
 import sqlite3
 import json
 import logging
-from sklearn.ensemble import RandomForestClassifier  
+from sklearn.ensemble import RandomForestClassifier   
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
